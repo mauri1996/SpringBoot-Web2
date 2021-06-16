af5701b07a31278afe657dacf0776ac0d409b678
mauri1996 <mauri1996@hotmail.es>
2021-06-16 13:28:40 +0200

    add service-config

---
+++ config-service/src/main/java/ucuenca/example/store/configservice/ConfigServiceApplication.java
@@ -0,0 +1,16 @@
+package ucuenca.example.store.configservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+
+@EnableConfigServer
+@SpringBootApplication
+public class ConfigServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigServiceApplication.class, args);
+	}
+
+}
