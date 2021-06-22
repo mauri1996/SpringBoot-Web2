5a70d491238c1ffc8a4923ff8e59cbf2c10c8895
mauri1996 <mauri1996@hotmail.es>
2021-06-22 11:44:22 -0500

    add product service

--- service-product/src/main/java/ucuenca/ejemplo/store/serviceproduct/ServiceProductApplication.java
+++ service-product/src/main/java/ucuenca/ejemplo/store/serviceproduct/ServiceProductApplication.java
@@ -2,8 +2,10 @@ package ucuenca.ejemplo.store.serviceproduct;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class ServiceProductApplication {
 
 	public static void main(String[] args) {
