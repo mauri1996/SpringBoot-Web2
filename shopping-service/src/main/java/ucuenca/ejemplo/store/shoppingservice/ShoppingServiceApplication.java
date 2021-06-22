67cdb115c3f40487cce5e69ea6be1a3d8f50a343
mauri1996 <mauri1996@hotmail.es>
2021-06-22 11:50:44 -0500

    add shopping service

--- shopping-service/src/main/java/ucuenca/ejemplo/store/shoppingservice/ShoppingServiceApplication.java
+++ shopping-service/src/main/java/ucuenca/ejemplo/store/shoppingservice/ShoppingServiceApplication.java
@@ -2,8 +2,10 @@ package ucuenca.ejemplo.store.shoppingservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class ShoppingServiceApplication {
 
 	public static void main(String[] args) {
